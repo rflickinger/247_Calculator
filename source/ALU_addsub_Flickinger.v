98fc046576604f130ad35218fd76aaf7536e7414
Ryan Flickinger <rflickinger15@gmail.com>
2023-04-08 16:31:39 -0400

    latest

--- source/ALU_addsub_Flickinger.v
+++ source/ALU_addsub_Flickinger.v
@@ -2,18 +2,31 @@
 
 module _ALU_addsub(
     input[7:0] A, B,
-    input Add_Sub, //sign_A, sign_B,
-    output[7:0] Sum,
-    output C_8, ALB, AGB, AEB//, Final_sign
+    input Add_Sub, sign_A, sign_B,
+    output[8:0] magnitude,
+    //output[7:0] Sum,
+    output C_8, ALB, AGB, AEB, Final_sign
     );
+    wire[7:0] precorrectSum;
+    wire[8:0] magOutWire;
+    
+    wire eop, eop_sign;
+    _EssOp_ID essential_op(Add_Sub, sign_A, sign_B, eop, eop_sign);    
+    
     //This add_sub module does pure binary addition or 2s comp subtraction
     //Leaves negative nums in 2s comp form
-
-    _8bitbinaddsub addsub( A[7:0], B[7:0], Add_Sub, Sum[7:0], C_8);
+    _8bitbinaddsub addsub( A[7:0], B[7:0], eop, precorrectSum[7:0], C_8);
     
     //This mag_com module compares the two 8 bit numbers to determine relative magnitude
     _8bitmagcom mag_com( A[7:0], B[7:0], 0, 0, 1, ALB, AGB, AEB);
     
-
+    //corrects the output to proper syntax
+    wire almost_sign;
+    //_Correcting corrector(Add_Sub, ALB, C_8, eop_sign, precorrectSum[7:0], almost_sign, magOutWire);
+    //_Correcting corrector(Add_Sub, ALB, C_8, eop_sign, precorrectSum[7:0], Final_sign, magOutWire);
+    _Correcting corrector(Add_Sub, ALB, C_8, eop_sign, precorrectSum[7:0], Final_sign, magnitude);
+    //assign Final_sign = almost_sign ^ eop_sign;
+    //assign C_8 = magOutWire[8];
+    //assign Sum = magOutWire[7:0];
     
 endmodule
---
+++ source/Correcting_Flickinger.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+
+module _Correcting(
+    input Add_Sub, ALB, C_8, eop_sign,
+    input[7:0] Sum,
+    output almost_sign,
+    output[8:0] magnitude
+    );
+    wire C8DP, SumCP, correctedC_8, correctedSumWire; 
+    
+    //sign from the original corrector
+    assign almost_sign = Add_Sub && ALB;
+    
+    //pass or zero out C_8. 1 means zero out C_8
+    assign C8DP = Add_Sub;
+    assign correctedC_8 = C8DP && C_8;
+    assign magnitude[8] = correctedC_8;
+    
+    // pass or complement sum
+    assign SumCP = Add_Sub && ALB;
+    wire[7:0] zeroHolder;
+    wire discardWire;
+    assign zeroHolder[7:0]  = 00000000;
+    _8bitbinaddsub corrector(zeroHolder, Sum, SumCP, magnitude[7:0], discardWire);
+
+endmodule
